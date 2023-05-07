1eb47a709c806f5a9105272396e6004beabd8833
Johan Andersson <johan-andersson01@protonmail.com>
2023-05-07 21:50:29 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake for developer & CI/CD environments";
+  description = "Flake for developer & CI/CD environments for cloud infrastructure";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -7,29 +7,35 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = nixpkgs.lib;
-        buildInputs = with pkgs; [ bash terraform ];
+        runtimeDeps = with pkgs; [ bash terraform ];
+        installDeps = with pkgs; [ makeWrapper ];
       in
       rec
       {
         devShell = pkgs.mkShell {
-          packages = buildInputs;
+          packages = runtimeDeps;
         };
 
         packages.ci = pkgs.stdenv.mkDerivation {
           name = "ci";
-          nativeBuildInputs = with pkgs; [ makeWrapper ];
-          buildInputs = buildInputs;
+
+          nativeBuildInputs = installDeps;
+          buildInputs = runtimeDeps;
+
           src = ./ci;
+
+          # Skip the other phases
           phases = [
             "installPhase"
           ];
 
-          # TODO: wrapper with dependencies?
+          # Install our CI/CD bash script, wrapped to get visibility of its
+          # dependencies' paths.
           installPhase = ''
             mkdir -p $out/bin
             install -t $out/bin $src/ci.sh
             wrapProgram $out/bin/ci.sh \
-              --prefix PATH : ${lib.makeBinPath buildInputs}
+              --prefix PATH : ${lib.makeBinPath runtimeDeps}
           '';
         };
 
